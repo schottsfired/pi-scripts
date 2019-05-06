17c2957c0e2785589e59cf19aeeb8561e11ea374
David Schott <schottdb@gmail.com>
2019-05-06 19:46:59 -0400

    Update bootstrap.sh

--- k3s-master/bootstrap.sh
+++ k3s-master/bootstrap.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -e
 
-#enable cgroups
+logger Updating packages and upgrading OS...
+apt-get update
+apt-get -y upgrade
+
 logger Enabling cgroups...
 sed -i 's/rootwait/cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory rootwait/g' /boot/cmdline.txt
 
-#static IP address
 logger Setting Static IP to 10.0.1.100 and Routher IP to 10.0.1.1...
 cat <<EOT >> /etc/dhcpcd.conf
 interface wlan0
@@ -14,6 +16,5 @@ static routers=10.0.1.1
 static domain_name_servers=10.0.1.1
 EOT
 
-#install k3s
 logger Installing k3s...
 curl -sfL https://get.k3s.io | sh -
