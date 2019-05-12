512f75bf1936a8f3d94e60ec1ef806623956767e
David Schott <schottdb@gmail.com>
2019-05-12 16:22:50 -0400

    Update bootstrap.sh

--- k3s-worker/bootstrap.sh
+++ k3s-worker/bootstrap.sh
@@ -16,5 +16,9 @@ static routers=10.0.1.1
 static domain_name_servers=10.0.1.1
 EOT
 
+echo Installing 3.5" LCD screen (requires restart)
+git clone https://github.com/goodtft/LCD-show
+./LCD-show/LCD35-show
+
 #Installing an agent to point at a server:
 #sudo curl -sfL https://get.k3s.io | K3S_TOKEN=xxx K3S_URL=https://10.0.1.100:6443 sh -
