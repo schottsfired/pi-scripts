93dc4086589816144cacbe3372ba61a1ff42dfd6
David Schott <schottdb@gmail.com>
2019-05-12 09:35:54 -0400

    Update bootstrap.sh

--- k3s-master/bootstrap.sh
+++ k3s-master/bootstrap.sh
@@ -21,6 +21,7 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--no-deploy=servicelb" sh -
 echo 'alias k="k3s kubectl"' >> ~/.bash_profile
 
 logger Install MetalLB...
-kubectl apply -f https://raw.githubusercontent.com/danderson/metallb/master/manifests/metallb.yaml
+kubectl apply -f https://raw.githubusercontent.com/schottsfired/pi-scripts/master/manifests/metallb.yaml
 
-logger Install configmap for MetalLB...
+logger Install ConfigMap for MetalLB...
+kubectl apply -f https://raw.githubusercontent.com/schottsfired/pi-scripts/master/manifests/metallb-layer2-config.yaml
