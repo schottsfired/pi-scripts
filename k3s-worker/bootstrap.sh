37abd78d761d7d690e2fcb5f26a6b8f4c4bbaae7
David Schott <schottdb@gmail.com>
2019-05-12 21:30:27 -0400

    Update bootstrap.sh

--- k3s-worker/bootstrap.sh
+++ k3s-worker/bootstrap.sh
@@ -16,13 +16,17 @@ static routers=10.0.1.1
 static domain_name_servers=10.0.1.1
 EOT
 
+echo Disable getting started wizard...
+rm /etc/xdg/autostart/piwiz.desktop
+
 echo Fix touch screen mouse tracking issue...
 apt-get install xserver-xorg-input-evdev
 cp -rf /usr/share/X11/xorg.conf.d/10-evdev.conf /usr/share/X11/xorg.conf.d/45-evdev.conf
 
 echo Install 3.5 inch LCD touch screen (script triggers restart)...
 git clone https://github.com/goodtft/LCD-show /home/pi/LCD-show
-cd /home/pi && source /home/pi/LCD-show/LCD35-show
+cd /home/pi
+source /home/pi/LCD-show/LCD35-show
 
 #NODE_TOKEN comes from /var/lib/rancher/k3s/server/node-token on the master
 #sudo curl -sfL https://get.k3s.io | K3S_TOKEN=xxx K3S_URL=https://10.0.1.100:6443 sh -
